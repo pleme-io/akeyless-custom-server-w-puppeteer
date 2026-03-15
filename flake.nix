9de18a09979fb0a005f0e48158726ffdff69a380
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:08 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Akeyless Custom Server with Puppeteer for browser-based credential management";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.default = pkgs.buildNpmPackage {
+        pname = "akeyless-custom-server-w-puppeteer";
+        version = "0.0.0-dev";
+        src = self;
+        sourceRoot = "source/node-app";
+        npmDepsHash = "sha256-SBfHd2Jo5uII5vBAmTcMIGgstY3lg6b460zBcPpkzZg="; # TODO: set correct hash
+        dontNpmBuild = true;
+        meta = {
+          description = "Akeyless Custom Server with Puppeteer for browser-based credential management";
+          homepage = "https://github.com/pleme-io/akeyless-custom-server-w-puppeteer";
+          license = pkgs.lib.licenses.isc;
+        };
+      };
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ nodejs_22 ];
+      };
+    });
+}
