b0f4727b7b8879d2215a64c5a6bf5d72620a9fa0
drzln <luis.d@akeyless.io>
2026-06-08 17:25:15 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Akeyless Custom Server with Puppeteer for browser-based credential management";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.follows = "substrate/nixpkgs";
     substrate = {
       url = "github:pleme-io/substrate";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     pleme-linker = {
       url = "github:pleme-io/pleme-linker";
